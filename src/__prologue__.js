a9e66dffdd1c04af73dd2b719da264d00a2b9b4e
Leonardo Val <leonardo.val@creatartis.com>
2017-08-16 10:05:05 -0300

    Moved sources from `ludorum-gamepack`

---
+++ src/__prologue__.js
@@ -0,0 +1,24 @@
+﻿/** Package wrapper and layout.
+*/
+function __init__(base, Sermat, ludorum) { "use strict";
+// Import synonyms. ////////////////////////////////////////////////////////////////////////////////
+	var declare = base.declare,
+		obj = base.obj,
+		copy = base.copy,
+		raise = base.raise,
+		raiseIf = base.raiseIf,
+		Iterable = base.Iterable,
+		iterable = base.iterable,
+		Game = ludorum.Game,
+		Checkerboard = ludorum.utils.Checkerboard,
+		CheckerboardFromString = ludorum.utils.CheckerboardFromString,
+		UserInterface = ludorum.players.UserInterface;
+
+// Library layout. /////////////////////////////////////////////////////////////////////////////////
+	var exports = {
+		__package__: 'ludorum-game-reversi',
+		__name__: 'ludorum_game_reversi',
+		__init__: __init__,
+		__dependencies__: [base, Sermat, ludorum],
+		__SERMAT__: { include: [base, ludorum] }
+	};
