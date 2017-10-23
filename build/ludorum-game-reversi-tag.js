b42d777724348ecc324f45a2b26bb771ac5b12d8
Leonardo Val <leonardo.val@creatartis.com>
2017-10-23 11:53:02 -0300

    New build

--- build/ludorum-game-reversi-tag.js
+++ build/ludorum-game-reversi-tag.js
@@ -1,5 +1,5 @@
 (function (init) { "use strict";
-			this["ludorum-game-reversi"] = init(this.base,this.Sermat,this.ludorum,this.playtester);
+			this["ludorum-game-reversi"] = init(this.base,this.Sermat,this.ludorum);
 		}).call(this,/** Package wrapper and layout.
 */
 function __init__(base, Sermat, ludorum) { "use strict";
